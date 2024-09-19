cf19ee64753bbfce2c96ee4f69696cdcd80d03d9
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2024-09-18 21:25:25 -0400

    final commit

--- checklist.py
+++ checklist.py
@@ -19,11 +19,40 @@ def destroy(index):
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print(index + list_item)
+        print("{} {}".format(index, list_item))
         index += 1
 
+def mark_completed(index):
+    checklist[index] = ("{} {}".format("√", checklist[index]))
+    return checklist
 
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
 
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+    
+    # Catch all
+    else:
+        print("Unknown Option")
+    
 def test():
     # Your tests here
     create("purple sox")
@@ -33,7 +62,23 @@ def test():
     print(read(1))
 
     update(0, "purple socks")
+
     destroy(1)
 
     print(read(0))
-    print(read(1))
+
+    list_all_items()
+
+    # Call your new function with the appropriate value
+    select("C")
+    # View the results
+    list_all_items()
+    # Call function with new value
+    select("R")
+    # View results
+    list_all_items()
+    # Continue until all code is run
+
+    user_value = user_input("Please, Enter a value:")
+    int(user_value)
+    print(user_value)
